d86f8538864a3257ef17c041bf9cea88c5f7b48d
Raphael Lopes <ph.campanario07@gmail.com>
2019-04-25 15:27:00 -0300

    Create session controller

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,23 @@
+const User = require('../models/User');
+
+const { errorMessage } = require('../utils');
+
+class SessionController {
+  async store (req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json(errorMessage('User not found'));
+    }
+
+    if (!await user.compareHash(password)) {
+      return res.status(400).json(errorMessage('Invalid password'));
+    }
+
+    return res.json({ user });
+  }
+};
+
+module.exports = new SessionController();
