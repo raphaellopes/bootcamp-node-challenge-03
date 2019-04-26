d96855c66ec82fc0d03706d7e252988793e43564
Raphael Lopes <ph.campanario07@gmail.com>
2019-04-26 12:29:33 -0300

    Create service email

---
+++ src/app/services/Mail.js
@@ -0,0 +1,23 @@
+// vendors
+const nodemailer = require('nodemailer');
+const path = require('path');
+const hbs = require('nodemailer-express-handlebars');
+const exphbs = require('express-handlebars');
+
+// locals
+const mailConfig = require('../../config/mail');
+
+const transport = nodemailer.createTransport(mailConfig);
+
+const viewPath = path.resolve(__dirname, '..', 'views', 'emails');
+
+// middlewares
+transport.use('compile', hbs({
+  viewEngine: exphbs.create({
+    partialsDir: path.resolve(viewPath, 'partials')
+  }),
+  viewPath,
+  extName: '.hbs'
+}));
+
+module.exports = transport;
