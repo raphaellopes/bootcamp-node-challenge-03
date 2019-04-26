0c9c71be306e1a5a67bdd2a4a28458f35c3e169a
Raphael Lopes <ph.campanario07@gmail.com>
2019-04-26 13:02:18 -0300

    Create valitation

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,46 @@
 // vendors
 const express = require('express');
+const validate = require('express-validation');
 
 // locals
-const {
-  UserController,
-  SessionController,
-  AdController,
-  PurchaseController
-} = require('./app/controllers');
+const validators = require('./app/validators');
+const controllers = require('./app/controllers');
 const authMiddleware = require('./app/middlewares/auth');
 
 const routes = express.Router();
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+);
+routes.post(
+  '/sessions',
+  validate(validators.User),
+  controllers.SessionController.store
+);
 
 // Ads
 routes.use(authMiddleware);
-routes.get('/ads', AdController.index);
-routes.get('/ads/:id', AdController.show);
-routes.post('/ads', AdController.store);
-routes.put('/ads/:id', AdController.update);
-routes.delete('/ads/:id', AdController.destroy);
+routes.get('/ads', controllers.AdController.index);
+routes.get('/ads/:id', controllers.AdController.show);
+routes.post(
+  '/ads',
+  validate(validators.Ad),
+  controllers.AdController.store
+);
+routes.put(
+  '/ads/:id',
+  validate(validators.Ad),
+  controllers.AdController.update
+);
+routes.delete('/ads/:id', controllers.AdController.destroy);
 
 // Purchases
-routes.post('/purchases', PurchaseController.store);
+routes.post(
+  '/purchases',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+);
 
 module.exports = routes;
